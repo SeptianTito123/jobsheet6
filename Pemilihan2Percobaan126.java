c8aa48cb9f749dd02bbbff41f839736fa24d281b
SeptianTito123 <SeptianTito123>
2023-10-05 13:34:16 +0700

    commit

--- Pemilihan2Percobaan126.java
+++ Pemilihan2Percobaan126.java
@@ -6,9 +6,16 @@ public class Pemilihan2Percobaan126 {
         System.out.print("Masukan Tahun   :");
         int tahun = input26.nextInt();
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if (tahun % 100 == 0) {
+            if (tahun % 400 == 0)  {
                 System.out.println("Tahun Kabisat");
-        } else
+        } else {
             System.out.println("Bukan Tahun Kabisat");
-    }
+        }
+    } else {
+        System.out.println("Tahun kabisat");
+    }    
+} else 
+        System.out.println("Bukan Tahun Kabisat");
+}
 }
